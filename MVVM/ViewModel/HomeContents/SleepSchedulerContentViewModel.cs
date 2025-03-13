9a907fa0fda2f662c845fdef28ebfa9e7a20f5d2
Bartek <bartlomiej00wilk@gmail.com>
2025-03-13 18:33:17 +0100

    sleep scheduler fixes

--- MVVM/ViewModel/HomeContents/SleepSchedulerContentViewModel.cs
+++ MVVM/ViewModel/HomeContents/SleepSchedulerContentViewModel.cs
@@ -1,6 +1,32 @@
-﻿namespace FitnessStudio.MVVM.ViewModel.HomeContents
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace FitnessStudio.MVVM.ViewModel.HomeContents
 {
-    public class SleepSchedulerContentViewModel
+    public partial class SleepSchedulerContentViewModel : ObservableObject
     {
+
+        //Properties for sleepscheduler vm
+        [ObservableProperty]
+        private bool isNotificationPopupOpen;
+
+        [ObservableProperty]
+        private bool isHolidayPopupOpen;
+
+        [ObservableProperty]
+        private bool isAlarmPopupOpen;
+
+        //Commands for sleepscheduler vm
+        public RelayCommand ShowNotificationPopupCommand { get; }
+        public RelayCommand ShowHolidayPopupCommand { get; }
+        public RelayCommand ShowAlarmPopupCommand { get; }
+
+        public SleepSchedulerContentViewModel()
+        {
+            ShowNotificationPopupCommand = new RelayCommand(() => IsNotificationPopupOpen = !IsNotificationPopupOpen);
+            ShowHolidayPopupCommand = new RelayCommand(() => IsHolidayPopupOpen = !IsHolidayPopupOpen);
+            ShowAlarmPopupCommand = new RelayCommand(() => IsAlarmPopupOpen = !IsAlarmPopupOpen);
+        }
+
     }
 }
